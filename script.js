eb8af3862fd95a16f98a36564def6951bde3719d
adam hovda <adamhovda@gmail.com>
2022-12-26 11:45:19 -0700

    Added checks for password

--- script.js
+++ script.js
@@ -21,38 +21,46 @@ function generatePassword(){
   let requiredPassChar = "";
   
   let passLength = prompt("Choose a password length between 8 and 128");
+    if(passLength >= 8 && passLength <= 128){
   
-        let lowerAlpa = confirm("Include lower case characters?");
-        if (lowerAlpa){
-          requiredPassChar += charOptions.lowerAlpa;
-        };
-        
-        let upperAlpha = confirm("Include upper case characters?");
-        if (upperAlpha){
-          requiredPassChar += charOptions.upperAlpha;
-        };
-        
-        let numbers = confirm("Include numbers?");
-        if (numbers) {
-          requiredPassChar += charOptions.numbers;
-        };
-        
-        let specialCharacters = confirm("Include special characters");
-        if (specialCharacters) {
-          requiredPassChar += charOptions.specialCharacters;
-        };
-        
-        console.log(requiredPassChar);
-        
-        let password = "";
-        for (let i = 0; i < passLength; i++){
-          password += requiredPassChar[Math.floor(Math.random() * requiredPassChar.length)]
+          let lowerAlpa = confirm("Include lower case characters?");
+          if (lowerAlpa){
+            requiredPassChar += charOptions.lowerAlpa;
+          };
+          
+          let upperAlpha = confirm("Include upper case characters?");
+          if (upperAlpha){
+            requiredPassChar += charOptions.upperAlpha;
+          };
+          
+          let numbers = confirm("Include numbers?");
+          if (numbers) {
+            requiredPassChar += charOptions.numbers;
+          };
+          
+          let specialCharacters = confirm("Include special characters");
+          if (specialCharacters) {
+            requiredPassChar += charOptions.specialCharacters;
+          };
+
+          
+          console.log(requiredPassChar);
+
+          if (lowerAlpa === false && upperAlpha === false && numbers === false && specialCharacters === false){
+            alert("You must select at least one criteria.  Please click Generate Password again")
+          } else{
+          
+          let password = "";
+          for (let i = 0; i < passLength; i++){
+            password += requiredPassChar[Math.floor(Math.random() * requiredPassChar.length)]
+          }
+
+          return password;
         }
-        return password;
+      } else {
+        alert("Invalid password length.  Please click Generate Password again");
 
-        console.log(password);
-
-
+      }
 };
 
 // Write password to the #password input
